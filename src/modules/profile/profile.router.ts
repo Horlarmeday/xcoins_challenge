edac026fba39826d29489c918088e659e501b360
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:08:29 +0100

    feat: Add profile routes

---
+++ src/modules/profile/profile.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { ProfileController } from "./profile.controller";
+
+const router = Router();
+
+router.route('/')
+    .get(ProfileController.getProfiles)
+    .post(ProfileController.findOrCreateProfile);
+
+export default router;
