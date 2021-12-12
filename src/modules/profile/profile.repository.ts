03aebfc76df509418c1fd4677041cf1919aabcef
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:08:16 +0100

    feat: Add profile repository to access data from database

---
+++ src/modules/profile/profile.repository.ts
@@ -0,0 +1,12 @@
+import { Profile } from "../../models/Profile";
+
+export const createProfile = async(data) => {
+    const { email, name, nickname } = data;
+    return await Profile.create({ email, name, nickname });
+};
+
+export const getProfiles = async() => await Profile.find().lean();
+
+export const getProfile = async(email: string, nickname: string) => {
+    return await Profile.findOne({$or: [{email}, {nickname}]}).exec();
+};
