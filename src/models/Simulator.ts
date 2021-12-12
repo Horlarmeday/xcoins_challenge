030c525de0b44e47036c81246ec3544c324f9c0e
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:02:03 +0100

    chore: Modify Simulator model

---
+++ src/models/Simulator.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const schema = new Schema(
+  {
+    profile_id: { type: Schema.Types.ObjectId, ref: 'Profile' },
+    dateRecorded: { type: Date },
+    cryptocurrency: { type: String },
+    euros: { type: Number },
+    price: { type: Number },
+    quantity: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Simulator = mongoose.model("Simulator", schema);
