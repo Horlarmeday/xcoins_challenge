f0594ade47daf79baa7dccccb363f91956377cef
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:05:09 +0100

    feat: Add startup loaders

---
+++ src/core/startup/loaders.ts
@@ -0,0 +1,10 @@
+import cors from "cors";
+import bodyParser from "body-parser";
+import express from "express";
+import { CORS_ORIGINS } from "../../config";
+
+export default (server: express.Application) => {
+    server.use(cors({ origin: CORS_ORIGINS }));
+    server.use(bodyParser.urlencoded({ extended: false }));
+    server.use(bodyParser.json());
+}
