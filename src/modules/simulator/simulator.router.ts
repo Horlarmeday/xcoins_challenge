cb92655593e09e06f5d96002b5000b8f0e957747
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:06:35 +0100

    feat: Add simulator routes

---
+++ src/modules/simulator/simulator.router.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { SimulatorController } from "./simulator.controller";
+
+const router = Router();
+
+router.route('/:profile_id')
+    .get(SimulatorController.getSimulator)
+    .post(SimulatorController.createSimulator);
+
+router.get('/', SimulatorController.getSimulators);
+
+export default router;
