f6209b4d37636c015ff5c6f469726373653a485c
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:03:33 +0100

    feat: Validate request to create profile

---
+++ src/modules/profile/validation/profile.validation.ts
@@ -0,0 +1,10 @@
+import Joi from "joi"
+
+export function validateProfile(profile) {
+    const schema = Joi.object({
+        email: Joi.string().min(6).max(255).required().email(),
+        nickname: Joi.string().required(),
+        name: Joi.string().required(),
+    });
+    return schema.validate(profile);
+}
