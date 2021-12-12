91333a074f514678c678872ab47afde612593a5f
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:06:17 +0100

    feat: Add simulator service class

---
+++ src/modules/simulator/simulator.service.ts
@@ -0,0 +1,20 @@
+import { createSimulator, getSimulator, getSimulators } from "./simulator.repository";
+import { Simulator } from "./simulator.interface";
+import {BadException} from "../../common/util/BadException";
+
+export class SimulatorService {
+    static async getSimulators(): Promise<Simulator[]> {
+        return getSimulators()
+    }
+
+    static async getSimulator(profile_id: string): Promise<Simulator> {
+        const simulator = await getSimulator(profile_id);
+        if (!simulator) throw new BadException(404, "Cannot find resource");
+        return simulator;
+    }
+
+    static async createSimulator(profile_id: string, body) {
+        const newSimulator = { ...body, profile_id };
+        return createSimulator(newSimulator)
+    }
+}
