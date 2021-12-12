26bb35de6b4b8087788a4477cda6f153d6329420
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:03:18 +0100

    feat: Validate request to create simulator

---
+++ src/modules/simulator/validation/simulator.validation.ts
@@ -0,0 +1,12 @@
+import Joi from "joi"
+
+export function validateSimulator(simulator) {
+    const schema = Joi.object({
+        price: Joi.string().required(),
+        cryptocurrency: Joi.string().required(),
+        dateRecorded: Joi.date().required(),
+        euros: Joi.number().required(),
+        quantity: Joi.number().required(),
+    });
+    return schema.validate(simulator);
+}
