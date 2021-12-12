13a2bdc7d028f44bc2f10534360eee38beb7bb69
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:10:35 +0100

    feat: Add favorites controller

---
+++ src/modules/favorites/favorites.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express"
+import { successResponse } from "../../common/responses/success.response";
+import { FavoritesService } from "./favorites.service";
+import { SUCCESS } from "../../common/constants";
+import { StatusCodes } from "../../common/constants/statusCodes";
+import { errorResponse } from "../../common/responses/error.response";
+
+export class FavoritesController {
+    static async getFavorites(req: Request, res: Response, next: NextFunction) {
+        try {
+            const favorites = await FavoritesService.getFavorites();
+            return successResponse({ res, data: favorites, message: SUCCESS, statusCode: StatusCodes.OK  })
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    static async getFavorite(req: Request, res: Response, next: NextFunction) {
+        const { profile_id } = req.params;
+        if (!profile_id) return errorResponse({ res, statusCode: StatusCodes.BAD_REQUEST, message: "Invalid profile id" });
+
+        try {
+            const favorite = await FavoritesService.getFavorite(profile_id);
+            return successResponse({ res, data: favorite, message: SUCCESS, statusCode: StatusCodes.OK })
+        } catch (e) {
+            next(e)
+        }
+    }
+}
