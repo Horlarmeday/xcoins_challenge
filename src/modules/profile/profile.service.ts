6c78ab343d51c7039297c04b00706a04789779f4
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:08:43 +0100

    feat: Add profile service class

---
+++ src/modules/profile/profile.service.ts
@@ -0,0 +1,17 @@
+import { createProfile, getProfile, getProfiles } from "./profile.repository";
+import { Profile } from "./profile.interface";
+
+export class ProfileService {
+    static async getProfiles(): Promise<Profile[]> {
+        return getProfiles()
+    }
+
+    static async findOrCreateProfile(body): Promise<Profile> {
+        const { email, nickname } = body;
+
+        let profile = await getProfile(email, nickname);
+        if (!profile) profile = await createProfile(body);
+
+        return profile
+    }
+}
