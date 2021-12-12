b5fafc7dcf3a71761141b9de189a78d1e8b1b50a
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:01:45 +0100

    chore: Modify Profile model

---
+++ src/models/Profile.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  name: { type: String, required: true},
+  nickname: { type: String, required: true},
+  email: { type: String, required: true},
+  capital: { type: Number, optional: true},
+  divisa: { type: String, optional: true},
+  prefered_cryptocurrency: { type: String, optional: true},
+});
+
+export const Profile = mongoose.model("Profile", schema);
