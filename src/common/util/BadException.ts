06f9fa40dd83698f308d99f3d229c5bb73d12bfd
Mahmud <ajaomahmud@gmail.com>
2021-12-12 23:56:21 +0100

    feat: Add error response class

---
+++ src/common/util/BadException.ts
@@ -0,0 +1,11 @@
+export class BadException extends Error {
+    private statusCode: number;
+    constructor(statusCode, message) {
+        super(message);
+        Object.setPrototypeOf(this, new.target.prototype);
+
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this);
+    }
+}
