c141387734f97de0bc69d10b9bed31ea07da4aad
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:07:02 +0100

    feat: Add simulator repository to access database

---
+++ src/modules/simulator/simulator.repository.ts
@@ -0,0 +1,18 @@
+import {Simulator} from "../../models/Simulator";
+import {Simulator as SimulatorInterface} from "./simulator.interface"
+
+export const getSimulators = async () => await Simulator.find().lean();
+
+export const getSimulator = async (profile_id: string) => await Simulator.findOne({ profile_id });
+
+export const createSimulator = async(data: SimulatorInterface) => {
+    const { price, cryptocurrency, dateRecorded, euros, profile_id, quantity } = data;
+    return await Simulator.create({
+        price,
+        cryptocurrency,
+        dateRecorded,
+        euros,
+        profile_id,
+        quantity
+    });
+};
