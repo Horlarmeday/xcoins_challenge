ea587cd469c266e10c09dd5156b3338be9d5f238
Mahmud <ajaomahmud@gmail.com>
2021-12-13 00:00:08 +0100

    feat: Add error middleware

---
+++ src/middleware/error.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Response, Request } from 'express';
+
+const handleError = (err: any, res: Response) => {
+    const { statusCode, message } = err;
+    const httpCode = statusCode || 500;
+    return res.status(httpCode).json({
+        success: false,
+        statusCode: httpCode,
+        message: httpCode === 500 ? 'Internal server error' : message,
+    });
+};
+
+export default function(error, req: Request, res: Response, next: NextFunction) {
+    handleError(error, res);
+}
