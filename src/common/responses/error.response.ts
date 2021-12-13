456fd83825b213c6e856d5c3b521e90c241afbe7
Mahmud <ajaomahmud@gmail.com>
2021-12-13 09:29:57 +0100

    chore: Prettify code

--- src/common/responses/error.response.ts
+++ src/common/responses/error.response.ts
@@ -1,15 +1,15 @@
-import {Response} from "express";
+import { Response } from 'express';
 
 type ErrorResponse = {
-    res: Response;
-    statusCode: number;
-    message: string
-}
+  res: Response;
+  statusCode: number;
+  message: string;
+};
 
 export const errorResponse = ({ res, statusCode, message }: ErrorResponse) => {
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message: statusCode === 500 ? 'internal server error' : message,
-    });
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message: statusCode === 500 ? 'internal server error' : message,
+  });
 };
